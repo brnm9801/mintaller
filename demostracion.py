2c297628b37782b814bb2c7e4980e552036fcc94
brnm9801 <gang8hn1@estudiantec.cr>
2024-09-20 01:06:13 -0600

    mintaller

--- demostracion.py
+++ demostracion.py
@@ -1,10 +1,21 @@
 import numpy as np
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageGrab
+import cv2
+import pyautogui
+import time
+from pyvirtualdisplay import Display
+import os
 
+os.environ['QT_QPA_PLATFORM'] = 'wayland'
+
+display = Display(visible=0, size=(1024, 768))
+display.start()
+#display = Display(visible=0, size=(1024, 768))
+#display.start()
 
 def texto(imagen):
-    pytesseract.pytesseract.tesseract_cmd = '/bin/tesseract'  # In case using colab after installing above modules
+    pytesseract.pytesseract.tesseract_cmd = '/bin/tesseract'  
     image = cv2.imread(imagen)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -12,35 +23,39 @@ def texto(imagen):
     text = text.strip()
     print(text)
     cv2.imshow('PythonGeeks', image)
-    cv2.waitKey(0)  # Wait for a key press
-    cv2.destroyAllWindows()  # Close all OpenCV windows
+    cv2.waitKey(0)  
+    cv2.destroyAllWindows()  
     main()
 
 def main():
-    # Ask the user to select the area to capture
     print("Move the crosshair to the top-left corner of the area you want to capture and press Enter.")
     input()
     x1, y1 = pyautogui.position()
+    print(f"Top-left corner: ({x1}, {y1})")
 
     print("Move the crosshair to the bottom-right corner of the area you want to capture and press Enter.")
     input()
     x2, y2 = pyautogui.position()
+    print(f"Bottom-right corner: ({x2}, {y2})")
+
+    # Add a delay to ensure the screenshot is taken correctly
+    time.sleep(1)
 
     # Capture the screenshot of the selected area
     img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
 
-    # Convert screenshot to format OpenCV can use (RGB)
-    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-    
-    # Display and save the screenshot using OpenCV
-    cv2.imshow('Screenshot', img)
-    cv2.imwrite('screenshot.png', img)
-    print("adgh")
-    
-    
-    
+    # Check if the image is not empty
+    if img:
+        # Convert screenshot to format OpenCV can use (RGB)
+        #img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        
+        # Display and save the screenshot using OpenCV
+        cv2.imshow('Screenshot', img)
+        cv2.imwrite('screenshot.png', img)
+        
+        texto('screenshot.png')  
+    else:
+        print("Failed to capture screenshot.")
 
-    texto('screenshot.png')  # Corrected function call
-
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
